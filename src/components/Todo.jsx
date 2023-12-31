21294b25ee00271c7f172347777957c366741d1e
Manga <cyrusparaiso@gmail.com>
2023-12-31 17:01:49 +0800

    Fix Letters

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,28 +5,24 @@ import TodoList from "./TodoList";
 function Todo() {
   const [todos, setTodos] = useState([]);
 
-  //function to add items once user click add button
   const addTodo = (todo) => {
     const newTodos = [todo, ...todos];
     setTodos(newTodos);
     console.log(...todos);
   };
 
-  //function to update items once user click update button
   const updateTodo = (todoId, newValue) => {
     setTodos((prev) =>
       prev.map((item) => (item.id === todoId ? newValue : item))
     );
   };
 
-  //function to remove items once user click remove icons
   const removeTodo = (id) => {
     const removedArr = [...todos].filter((todo) => todo.id !== id);
 
     setTodos(removedArr);
   };
 
-  //function to show items as completed once user click on completed task
   const completeTodo = (id) => {
     let updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
@@ -39,10 +35,7 @@ function Todo() {
 
   return (
     <div>
-      <h1 className="header">Add your Plan for Today?</h1>
-
-      {/*once we display header to the webpage 
-      we will pass the function as props to TodoForm and TodoList to display the data to user*/}
+      <h1 className="header">Florendo | To Do List</h1>
 
       <TodoForm onSubmit={addTodo} />
       <TodoList
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,8 @@
 import React, { useState, useRef } from "react";
 
 function TodoForm(props) {
-  //we will check if user is tring to update then we will display the value from props otherwise we will display null or ""
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
-  //useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference.
-  //A reference is an object having a single property “current”, which can be accessed and changed (mutated).
   const inputRef = useRef(null);
 
   const handleChange = (e) => {
@@ -13,10 +10,8 @@ function TodoForm(props) {
   };
 
   const handleSubmit = (e) => {
-    // the preventDefault() method is used to prevent the default behavior of an event from occurring
     e.preventDefault();
 
-    //here we have provided id as random number which will help us while updating or deleting the task
     props.onSubmit({
       id: Math.floor(Math.random() * 10000),
       text: input,
@@ -27,13 +22,10 @@ function TodoForm(props) {
 
   return (
     <form className="todo_form">
-      {/* In react we use "?" for conditional statement like if-else.
-       we have used, props.edit ?(if) show update button : (else) show Add button */}
-
       {props.edit ? (
         <>
           <input
-            placeholder="Update your item"
+            placeholder="Update Listing"
             value={input}
             onChange={handleChange}
             name="text"
@@ -41,13 +33,13 @@ function TodoForm(props) {
             className="input-edit"
           />
           <button onClick={handleSubmit} className="edit-button">
-            Update
+            update
           </button>
         </>
       ) : (
         <>
           <input
-            placeholder="Add your plan to list"
+            placeholder="Add Listing"
             value={input}
             onChange={handleChange}
             name="text"
@@ -55,7 +47,7 @@ function TodoForm(props) {
             ref={inputRef}
           />
           <button onClick={handleSubmit} className="add-button">
-            Add
+            add
           </button>
         </>
       )}
